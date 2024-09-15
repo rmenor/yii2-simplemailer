6399efc188e22c8699ec704c4b0df9f51f9b1344
Ramón Menor <ramonmenor@gmail.com>
2024-09-15 14:48:19 +0200

    Commit inicial

---
+++ src/Mailer.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace rmenor\simplemailer;
+
+use PHPMailer\PHPMailer\PHPMailer;
+use yii\base\Component;
+
+class Mailer extends Component
+{
+    public $host;
+    public $username;
+    public $password;
+    public $port = 587;
+    public $encryption = PHPMailer::ENCRYPTION_STARTTLS;
+
+    public function sendMail($to, $subject, $body)
+    {
+        $mail = new PHPMailer(true);
+
+        try {
+            // Configuración del servidor SMTP
+            $mail->isSMTP();
+            $mail->Host       = $this->host;
+            $mail->SMTPAuth   = true;
+            $mail->Username   = $this->username;
+            $mail->Password   = $this->password;
+            $mail->SMTPSecure = $this->encryption;
+            $mail->Port       = $this->port;
+
+            // Destinatarios
+            $mail->setFrom($this->username, 'Mailer');
+            $mail->addAddress($to);
+
+            // Contenido del correo
+            $mail->isHTML(true);
+            $mail->Subject = $subject;
+            $mail->Body    = $body;
+
+            // Enviar el correo
+            $response = $mail->send();
+
+            if ($response) {
+                return [
+                    'success' => true,
+                    'message' => 'El mensaje fue enviado'
+                ];
+            } else {
+                return [
+                    'success' => false,
+                    'message' => 'El mensaje no pudo ser enviado. Error: ' . $mail->ErrorInfo
+                ];
+            }
+        } catch (\Exception $e) {
+            return [
+                'success' => false,
+                'message' => 'El mensaje no pudo ser enviado. Error: ' . $mail->ErrorInfo
+            ];
+        }
+    }
+}
